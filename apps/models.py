9daee3d712debe8f3968c3e1e2386cb695f1c97b
koladev <onaelmangabo@gmail.com>
2022-04-19 23:14:46 +0100

    🚧 add abstract model

---
+++ apps/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class AbstractModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
