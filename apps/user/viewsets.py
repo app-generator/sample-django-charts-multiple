430a529e6e68950b894d4b7f5dc6cb4f69855510
koladev <onaelmangabo@gmail.com>
2022-04-20 00:18:10 +0100

    🚧 add product model migration

--- apps/user/viewsets.py
+++ apps/user/viewsets.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import action
