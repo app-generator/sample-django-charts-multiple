da6bb7acb086f46be86f707473ba263a87bd9729
koladev <onaelmangabo@gmail.com>
2022-04-20 00:10:42 +0100

    🚧 register product endpoints

--- apps/routers.py
+++ apps/routers.py
@@ -1,11 +1,13 @@
 from rest_framework import routers
 
 from apps.user.viewsets import UserViewSet
+from apps.product.viewsets import ProductViewSet
 
 router = routers.SimpleRouter()
 
 
-router.register('users', UserViewSet)
+router.register('user', UserViewSet)
+router.register('product', ProductViewSet)
 
 urlpatterns = [
     *router.urls,
