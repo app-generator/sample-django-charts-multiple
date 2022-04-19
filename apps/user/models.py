62e45661d1ca0ccbe6f50d65c04de744e7a0cfe8
koladev <onaelmangabo@gmail.com>
2022-04-19 23:16:52 +0100

    🚧 add user model

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    The User model has already:
+    - has_purchased
+    """
+
+    has_purchased = models.BooleanField(default=False)
