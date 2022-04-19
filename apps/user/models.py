e1c83ef13d54207d73e8663dde38e74ae8f35501
koladev <onaelmangabo@gmail.com>
2022-04-19 23:17:06 +0100

    ✏️ fix typo

--- apps/user/models.py
+++ apps/user/models.py
@@ -3,9 +3,6 @@ from django.db import models
 
 
 class User(AbstractUser):
-    """
-    The User model has already:
-    - has_purchased
-    """
 
     has_purchased = models.BooleanField(default=False)
+    
